32fb240db58010a0010d752f7b4e8861e92c8305
Tim Cope <kirbycope@gmail.com>
2018-05-21 14:27:23 -0600

    Added last slide buttons for SDET Getting Started Guide

--- sdetqa/public/scripts/slide-controls.js
+++ sdetqa/public/scripts/slide-controls.js
@@ -19,6 +19,30 @@ function previousSlide() {
     }
 }
 
+function showLastSlideButtons() {
+    if (document.getElementById("buttonRow") === null) {
+        var buttonRow = document.createElement("div");
+        buttonRow.id = "buttonRow";
+        buttonRow.className = "container text-center";
+        document.getElementById("slideText").insertAdjacentElement("afterend", buttonRow);
+        var apiLink = document.createElement("a");
+        apiLink.className = "btn btn-danger";
+        apiLink.textContent = "API";
+        apiLink.href = "./api";
+        buttonRow.appendChild(apiLink);
+        var mobileLink = document.createElement("a");
+        mobileLink.className = "btn btn-warning";
+        mobileLink.textContent = "Mobile";
+        mobileLink.href = "./mobile";
+        buttonRow.appendChild(mobileLink);
+        var webLink = document.createElement("a");
+        webLink.className = "btn btn-success";
+        webLink.textContent = "Web";
+        webLink.href = "./web";
+        buttonRow.appendChild(webLink);
+    }
+}
+
 function updateSlideControls() {
 
     if (index + 1 == data.length) {
